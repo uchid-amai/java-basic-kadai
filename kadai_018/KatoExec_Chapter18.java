d15a3a93a9e0484ed2b1c007353e56c973e06e36
uchid-amai <mai836tnp@gmail.com>
2025-07-12 22:35:59 +0900

    kadai_18を提出

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,23 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+
+	public static void main(String[] args) { 
+		KatoTaro_Chapter18 taro = new KatoTaro_Chapter18();
+		taro.setGivenName();
+		taro.commonIntroduce();
+		taro.eachIntroduce();
+	
+
+		KatoIchiro_Chapter18 ichiro = new KatoIchiro_Chapter18();
+		ichiro.setGivenName();
+		ichiro.commonIntroduce();
+		ichiro.eachIntroduce();
+		
+		
+		KatoHanako_Chapter18 hanako = new KatoHanako_Chapter18();
+		hanako.setGivenName();
+		hanako.commonIntroduce();
+		hanako.eachIntroduce();
+	}
+}
---
+++ kadai_018/Kato_Chapter18.java
@@ -0,0 +1,18 @@
+package kadai_018;
+
+abstract class Kato_Chapter18 {
+	public String familyName = "加藤" ; //姓を表す
+	public String givenName ; //名を表す
+	public String address = "東京都中野区〇×" ;  //住所を表す
+	
+	public void commonIntroduce() {
+		System.out.println("名前は" + familyName +givenName + "です");
+		System.out.println("住所は" + address + "です");
+	}
+	
+	abstract public void eachIntroduce();
+	
+
+		
+	
+}
